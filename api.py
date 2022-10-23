bfa966f73b35663792e206ebd895914ef1b73772
LauraFlórez <59582182+florezal754@users.noreply.github.com>
2022-10-23 11:16:30 +0200

    Update api.py

--- api.py
+++ api.py
@@ -15,3 +15,24 @@ CORS(app)
 @app.route("/")
 def home():
 	return '<p>hola</p>'
+
+@app.route('/historical')
+def historical():
+    start_date = input('Introduce date: ')
+    end_date = input('Introduce date: ')
+    start_date = str(start_date)
+    end_date = str(end_date)
+    Dataset = pd.read_csv('penalty_signals.csv', encoding="UTF-8")
+    if start_date and end_date in Dataset.PBF_datetime_utc.values:
+        print("The date is in list")
+              
+        mask = (Dataset.PBF_datetime_utc >= start_date) & (Dataset.PBF_datetime_utc <= end_date)
+        filtered_df = Dataset.loc[mask]
+        response = filtered_df
+        response = filtered_df.to_json()
+	
+    else:
+        response = ['Requested datetime series is not available.']
+        print('olala')
+
+    return response 
